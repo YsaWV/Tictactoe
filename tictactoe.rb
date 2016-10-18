fc5ec078be4b9573181fb2f97954f5032217135f
Ysabel Bombardiere <ysabelestradab@gmail.com>
2016-10-18 16:12:45 -0400

    border working

--- tictactoe.rb
+++ tictactoe.rb
@@ -14,3 +14,28 @@ def border()
 end 
 
 border()
+
+
+
+
+
+#class TicTactoe
+
+	# def initialize
+ #  		@spaces = Array.new(9)
+	# end
+
+def to_s
+  		output = []
+    	0.upto(8) do |position|
+      		puts output << " #{spaces[position] || position} "
+      		case position % 3
+      			when 0, 1 then output << "|"
+      			when 2 then output << "\n-----------\n" unless position == 8
+      			puts output
+    		end
+  		end
+  	puts output
+end
+
+puts to_s()
