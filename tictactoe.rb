8df8b8a538032d3a629fdb2a75a62e75f54ac604
Ysabel Bombardiere <ysabelestradab@gmail.com>
2016-10-18 15:44:58 -0400

    added border

---
+++ tictactoe.rb
@@ -0,0 +1,16 @@
+# class TicTacToe()
+# 	def border()
+# 		puts ("___|___|___")
+# 	end 
+
+# end
+
+def border()
+		a = ("___|___|___")
+		b = ("   |   |   ")
+
+		2.times {puts a}
+		puts b
+end 
+
+border()
