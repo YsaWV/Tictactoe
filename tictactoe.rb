866da12e15a514ace133a2fce7f25d1c7b6aad17
Ysabel Bombardiere <ysabelestradab@gmail.com>
2016-10-18 22:25:38 -0400

    updated border and added X

--- tictactoe.rb
+++ tictactoe.rb
@@ -6,14 +6,27 @@
 # end
 
 def border()
-		a = ("___|___|___")
-		b = ("   |   |   ")
+ 		a = ("___|___|___")
+ 		b = ("   |   |   ")
 
-		2.times {puts a}
-		puts b
-end 
+ 		2.times {puts a}
+ 		puts b
+ # end 
+
+ # border()
+
+ # def fill()
+ 	c = ("_X_|___|___")
+
+ 	puts c
+ 	puts a
+ 	puts b
+
+
+ end
 
 border()
+ # fill()
 
 
 
@@ -25,17 +38,33 @@ border()
  #  		@spaces = Array.new(9)
 	# end
 
-def to_s
-  		output = []
-    	0.upto(8) do |position|
-      		puts output << " #{spaces[position] || position} "
-      		case position % 3
-      			when 0, 1 then output << "|"
-      			when 2 then output << "\n-----------\n" unless position == 8
-      			puts output
-    		end
-  		end
-  	puts output
-end
+# def to_s()
+#   	output = []
+#     0.upto(8) do |position|
+#       	# puts output << " #{spaces[position] || position} "
+#       	case position % 3
+#       		when 0, 1 then puts output << "|"
+#       		# when 2 then puts output << "\n-----------\n" unless position == 8
+#       	puts output
+#     	end
+#   	end
+#   	puts output
+# end
 
-puts to_s()
+
+
+# puts to_s()
+
+# def to_fill
+#   		output = []
+#     	0.upto(8) do |position|
+#       		puts output << " #{spaces[position] || position} "
+#       		case position % 3
+#       			when 0, 1 then output << "|"
+#       			when 2 then output << "\n-----------\n" unless position == 8
+#       			puts output
+#     		end
+#   		end
+#   	puts output
+# end
+
