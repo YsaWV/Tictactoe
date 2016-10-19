b1a6bd097b26d136456d299931bb82871897aed0
Ysabel Bombardiere <ysabelestradab@gmail.com>
2016-10-18 22:55:34 -0400

    added random X and O

---
+++ tictac.rb
@@ -0,0 +1,20 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+
+
+i = rand(0..8)
+board[i] = "X"
+board[i+1] = "O"
+
+display_board(board)
+
+
+
